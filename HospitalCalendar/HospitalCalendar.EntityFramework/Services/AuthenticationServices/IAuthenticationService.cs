36343ef7e5e1eb056cdc46eb579515c1b432933d
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-04-15 17:11:22 +0200

    Defined interfaces for User entity search and Authentication

---
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/AuthenticationServices/IAuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalCalendar.Domain.Services.AuthenticationServices
+{
+    public enum RegistrationResult
+    {
+        Success,
+        PasswordsDoNotMatch,
+        UsernameAlreadyExists
+    }
+
+    public interface IAuthenticationService
+    {
+        Task<RegistrationResult> Register<T>(string firstName, string lastName, string username, string password, string confirmPassword) where T : User, new();
+        Task<User> Login(string username, string password);
+    }
+}
