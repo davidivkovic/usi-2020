06228e93c717b2a1da07a1701b00266af43393a7
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-04-15 17:06:45 +0200

    Defined a CRUD interface for entities to interact with the database

---
+++ HospitalCalendar/HospitalCalendar.Domain/Services/IDataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalCalendar.Domain.Services
+{
+    public interface IDataService<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> Get(Guid ID);
+
+        Task<T> Create(T entity);
+
+        Task<T> Update(T entity);
+
+        Task<bool> Delete(Guid ID);
+    }
+}
