37d774043ff1590da1e2ca075c9cf4dbb02b7b5c
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-04-17 19:32:35 +0200

    Added exceptions for the UserDataService

---
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Exceptions/NonMatchingPasswordException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalCalendar.EntityFramework.Exceptions
+{
+    class NonMatchingPasswordException : Exception
+    {
+        public NonMatchingPasswordException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Exceptions/UsernameAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalCalendar.EntityFramework.Exceptions
+{
+    class UsernameAlreadyExistsException : Exception
+    {
+        public UsernameAlreadyExistsException(string message) : base(message)
+        {
+        }
+    }
+}
