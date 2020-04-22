07f2320f2c91249cc217711d5913c9bdfd88710d
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-04-22 20:22:50 +0200

    Created User Views and implemented their ViewModels

---
+++ HospitalCalendar/HospitalCalendar.WPF/Views/AdministratorMenu/RoomList.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HospitalCalendar.WPF.Views.AdministratorMenu
+{
+    /// <summary>
+    /// Interaction logic for RoomList.xaml
+    /// </summary>
+    public partial class RoomList : UserControl
+    {
+        public RoomList()
+        {
+            InitializeComponent();
+        }
+    }
+}
