a5b5541335a3483db75af4387c67369c15a7f1cb
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-04-22 20:21:38 +0200

    Refactored Authentication service

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/AuthenticationServices/AuthenticationService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/AuthenticationServices/AuthenticationService.cs
@@ -4,6 +4,7 @@ using HospitalCalendar.Domain.Services;
 using HospitalCalendar.Domain.Services.AuthenticationServices;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
+using HospitalCalendar.EntityFramework.Exceptions.HospitalCalendar.Domain.Exceptions;
 
 namespace HospitalCalendar.EntityFramework.Services.AuthenticationServices
 {
@@ -22,13 +23,18 @@ namespace HospitalCalendar.EntityFramework.Services.AuthenticationServices
         {
             var storedUser = await _userService.GetByUsername(username);
 
+            if (storedUser == null)
+            {
+                throw new InvalidUsernameException(username, password);
+            }
+            
             PasswordVerificationResult passwordResult = _passwordHasher.VerifyHashedPassword(storedUser, storedUser.Password, password);
 
             if (passwordResult != PasswordVerificationResult.Success)
             {
                 throw new InvalidPasswordException(username, password);
             }
-
+            
             return storedUser;
         }
     }
