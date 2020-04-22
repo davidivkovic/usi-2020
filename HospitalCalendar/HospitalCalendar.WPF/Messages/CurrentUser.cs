2432216dbc2e1f48e96fb097031375bbc8c6d500
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-04-22 20:25:13 +0200

    Implemented Logout

---
+++ HospitalCalendar/HospitalCalendar.WPF/Messages/CurrentUser.cs
@@ -0,0 +1,14 @@
+﻿using HospitalCalendar.Domain.Models;
+
+namespace HospitalCalendar.WPF.ViewModels
+{
+    internal class CurrentUser
+    {
+        public User User { get; set; }
+
+        public CurrentUser(User user)
+        {
+            User = user;
+        }
+    }
+}
