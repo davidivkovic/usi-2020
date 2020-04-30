26d49645ce0341f136997fa16adff436e855a5ea
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-04-30 22:33:15 +0200

    Implemented exceptions for the Equipment Menu

---
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Exceptions/EquipmentTypeAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalCalendar.EntityFramework.Exceptions
+{
+    public class EquipmentTypeAlreadyExistsException : Exception
+    {
+        public EquipmentTypeAlreadyExistsException(string message) : base(message)
+        {
+        }
+    }
+}
