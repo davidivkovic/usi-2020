0f7ba3b00ae8eef9dfa9abac2827095d3ee505ff
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-04-30 22:34:35 +0200

    Other changes

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/CalendarEntryServices/RenovationService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/CalendarEntryServices/RenovationService.cs
@@ -1,11 +1,45 @@
-﻿using HospitalCalendar.Domain.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HospitalCalendar.Domain.Models;
+using HospitalCalendar.Domain.Services.CalendarEntryServices;
+using Microsoft.EntityFrameworkCore;
 
 namespace HospitalCalendar.EntityFramework.Services.CalendarEntryServices
 {
-    public class RenovationService : GenericDataService<Renovation>
+    public class RenovationService : GenericDataService<Renovation>, IRenovationService
     {
         public RenovationService(HospitalCalendarDbContextFactory contextFactory) : base(contextFactory)
         {
         }
+
+        public Task<Renovation> Create(Room room, Room roomToAdd, RoomType newRoomType, DateTime start, DateTime end,
+            bool splitting, ICollection<EquipmentItem> removedEquipmentItems, ICollection<EquipmentItem> addedEquipmentItems)
+        {
+            throw new NotImplementedException();
+        }
+        /*
+public new async Task<Renovation> Create(List<EquipmentItem> addedEquipmentItems)
+{
+   using (HospitalCalendarDbContext context = ContextFactory.CreateDbContext())
+   {
+       addedEquipmentItems.ForEach(aei =>
+       {
+           var addedEquipmentItemsToUpdate = context.EquipmentItems
+               .FirstOrDefaultAsync(ei => ei.ID == aei.ID).Result;
+
+           addedEquipmentItemsToUpdate.
+       });
+
+
+       var createdRenovation = new Renovation()
+       {
+
+       };
+       return createdRenovation;
+   }
+}
+*/
     }
 }
