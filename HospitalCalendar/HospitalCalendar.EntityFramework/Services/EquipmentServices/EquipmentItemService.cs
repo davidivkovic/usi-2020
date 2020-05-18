f6e830803932769e672ca676d1b857d45150e577
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-18 18:39:30 +0200

    Extended and Refactored EquipmentItemService

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/EquipmentServices/EquipmentItemService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/EquipmentServices/EquipmentItemService.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using HospitalCalendar.Domain.Models;
+﻿using HospitalCalendar.Domain.Models;
 using HospitalCalendar.Domain.Services.EquipmentServices;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using HospitalCalendar.Domain.Services.CalendarEntryServices;
-using HospitalCalendar.EntityFramework.Services.CalendarEntryServices;
 
 namespace HospitalCalendar.EntityFramework.Services.EquipmentServices
 {
@@ -21,10 +18,10 @@ namespace HospitalCalendar.EntityFramework.Services.EquipmentServices
         {
             await using var context = ContextFactory.CreateDbContext();
             return await context.EquipmentItems
-                    .Where(ei => ei.IsActive)
-                    .Where(ei => ei.EquipmentType.Name == equipmentType.Name)
-                    .Include(ei => ei.EquipmentType)
-                    .ToListAsync();
+                .Where(ei => ei.IsActive)
+                .Where(ei => ei.EquipmentType.Name == equipmentType.Name)
+                .Include(ei => ei.EquipmentType)
+                .ToListAsync();
         }
 
         public async Task<ICollection<EquipmentItem>> GetAllInUseByType(EquipmentType equipmentType)
@@ -32,39 +29,39 @@ namespace HospitalCalendar.EntityFramework.Services.EquipmentServices
             return await Task.Run(async () =>
             {
                 await using var context = ContextFactory.CreateDbContext();
-                var equipmentItemsInRooms = context.Rooms
+
+                var equipmentItemsInRooms = await context.Rooms
                     .Include(r => r.Equipment)
-                        .ThenInclude(ei => ei.EquipmentType)
-                    .Select(r => r.Equipment)
-                    .FirstOrDefault()?
-                    .Where(ei => ei.EquipmentType.Name == equipmentType.Name)
-                    .ToList();
+                    .ThenInclude(ei => ei.EquipmentType)
+                    .Where(eir => eir.Equipment.Any(eee => eee.EquipmentType.Name == equipmentType.Name))
+                    .SelectMany(r => r.Equipment)
+                    .ToListAsync();
+
+                // TODO: If equipment count doesnt add up change the next lines to match the previous
 
                 // Added in renovations
                 var addedEquipmentItems = context.Renovations
-                    .Include(r=> r.AddedEquipmentItems)
-                        .ThenInclude(ei => ei.EquipmentType)
-                    .Select(x=> x.AddedEquipmentItems)
-                    .FirstOrDefault()?
+                    .Include(r => r.AddedEquipmentItems)
+                    .ThenInclude(ei => ei.EquipmentType)
+                    .SelectMany(x => x.AddedEquipmentItems)
                     .Where(aei => aei.EquipmentType.Name == equipmentType.Name)
                     .ToList();
 
                 // Removed in renovations
                 var removedEquipmentItems = context.Renovations
                     .Include(r => r.RemovedEquipmentItems)
-                        .ThenInclude(ei => ei.EquipmentType)
-                    .Select(x => x.RemovedEquipmentItems)
-                    .FirstOrDefault()?
+                    .ThenInclude(ei => ei.EquipmentType)
+                    .SelectMany(x => x.RemovedEquipmentItems)
                     .Where(rei => rei.EquipmentType.Name == equipmentType.Name)
                     .ToList();
 
-                var emptyList = Enumerable.Empty<EquipmentItem>();
+                //var emptyList = Enumerable.Empty<EquipmentItem>();
 
-                var allItemsInUse = equipmentItemsInRooms ?? emptyList;
-                allItemsInUse = allItemsInUse?.Union(addedEquipmentItems ?? emptyList); 
-                allItemsInUse = allItemsInUse?.Union(removedEquipmentItems ?? emptyList);
+                var allItemsInUse = equipmentItemsInRooms;
+                allItemsInUse = allItemsInUse.Union(addedEquipmentItems).ToList();
+                allItemsInUse = allItemsInUse.Union(removedEquipmentItems).ToList();
 
-                var allItemsInUseDistinct = allItemsInUse?
+                var allItemsInUseDistinct = allItemsInUse
                     .GroupBy(ei => ei.ID)
                     .Select(g => g.First())
                     .ToList();
@@ -81,9 +78,9 @@ namespace HospitalCalendar.EntityFramework.Services.EquipmentServices
             return await Task.Run(() =>
             {
                 var freeEquipmentItems = allEquipmentItems
-                .Where(ei => equipmentItemsInUse
-                    .All(eiu => eiu.ID != ei.ID))
-                .ToList();
+                    .Where(ei => equipmentItemsInUse
+                        .All(eiu => eiu.ID != ei.ID))
+                    .ToList();
                 return freeEquipmentItems;
             });
         }
@@ -92,44 +89,73 @@ namespace HospitalCalendar.EntityFramework.Services.EquipmentServices
         {
             await using var context = ContextFactory.CreateDbContext();
 
-            return await Task.Run(async() =>
+            return await Task.Run(async () =>
             {
                 var createdEquipmentItems = Enumerable
                     .Range(1, amount)
-                    .Select(i => new EquipmentItem{IsActive =  true})
+                    .Select(i => new EquipmentItem {IsActive = true})
                     .ToList();
 
                 context.EquipmentItems.AddRange(createdEquipmentItems);
                 await context.SaveChangesAsync();
 
-                createdEquipmentItems.ForEach(ei=>ei.EquipmentType = equipmentType);
+                createdEquipmentItems.ForEach(ei => ei.EquipmentType = equipmentType);
                 context.EquipmentItems.UpdateRange(createdEquipmentItems);
                 await context.SaveChangesAsync();
 
                 return true;
             });
         }
-        
+
         public async Task<bool> Remove(EquipmentType equipmentType, int amount)
         {
+
             await using var context = ContextFactory.CreateDbContext();
 
             var freeEquipmentItems = await GetAllFreeByType(equipmentType);
             freeEquipmentItems = freeEquipmentItems.Take(Math.Abs(amount)).ToList();
-
-            context.EquipmentItems.RemoveRange(freeEquipmentItems);
-            await context.SaveChangesAsync();
-
+            await Task.Factory.StartNew(async () =>
+            {
+                context.EquipmentItems.RemoveRange(freeEquipmentItems);
+                await context.SaveChangesAsync();
+            });
             return true;
+        }
 
-        public async Task<ICollection<EquipmentItem>> GetAllWithoutRoom() 
+        public async Task<ICollection<EquipmentItem>> GetAllWithoutRoom()
         {
-            using (HospitalCalendarDbContext context = _contextFactory.CreateDbContext())
+            using (HospitalCalendarDbContext context = ContextFactory.CreateDbContext())
             {
                 return await context.EquipmentItems
-                                    .Where(ei => ei.IsActive)
-                                    .Where(ei => ei.Room == null)
-                                    .ToListAsync();
+                    .Where(ei => ei.IsActive)
+                    .Where(ei => ei.Room == null)
+                    .ToListAsync();
+            }
+        }
+
+        public async Task<ICollection<EquipmentItem>> GetAllByRoom(Room room)
+        {
+            using (HospitalCalendarDbContext context = ContextFactory.CreateDbContext())
+            {
+                return await context.EquipmentItems
+                    .Where(ei => ei.IsActive)
+                    .Where(ei => ei.Room.ID == room.ID)
+                    .ToListAsync();
+            }
+        }
+
+        public async Task<ICollection<EquipmentItem>> GetAllByTypeInRoom(EquipmentType equipmentType, Room room)
+        {
+            await using (HospitalCalendarDbContext context = ContextFactory.CreateDbContext())
+            {
+                return context.Rooms
+                    .Where(r => r.IsActive)
+                    .Include(r => r.Equipment)
+                    .ThenInclude(e => e.EquipmentType)
+                    .First(r => r.ID == room.ID)
+                    .Equipment
+                    .Where(ei => ei.EquipmentType.Name == equipmentType.Name)
+                    .ToList();
             }
         }
     }
