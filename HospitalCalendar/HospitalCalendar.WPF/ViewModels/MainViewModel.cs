091f135db1f3417a0a4a82ce3d296c0654e3505a
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-18 18:49:33 +0200

    Refactored MainViewModel on base of IL Weaving

--- HospitalCalendar/HospitalCalendar.WPF/ViewModels/MainViewModel.cs
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/MainViewModel.cs
@@ -10,92 +10,23 @@ using HospitalCalendar.WPF.ViewModels.Login;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu;
 using MaterialDesignColors;
 using MaterialDesignThemes.Wpf;
+using PropertyChanged;
 
 namespace HospitalCalendar.WPF.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        #region Properties
-        private ViewModelBase _currentViewModel;
-        private User _currentUser;
-        private bool _darkModeIsEnabled;
+        public User CurrentUser { get; set; }
+         
+        public bool DarkModeIsEnabled { get; set; }
 
         public ICommand Logout { get; set; }
-
-        public User CurrentUser
-        {
-            get => _currentUser;
-
-            set
-            {
-                if (_currentUser == value)
-                    return;
-                _currentUser = value;
-                RaisePropertyChanged(nameof(CurrentUser));
-            }
-
-        }
-
-        public ViewModelBase CurrentViewModel
-        {
-            get => _currentViewModel;
-
-            set
-            {
-                if (_currentViewModel == value)
-                    return;
-                _currentViewModel = value;
-                RaisePropertyChanged(nameof(CurrentViewModel));
-                RaisePropertyChanged(nameof(CurrentUserFirstName));
-                RaisePropertyChanged(nameof(CurrentUserLastName));
-            }
-        }
-
-        public string CurrentUserFirstName
-        {
-            get => CurrentUser?.FirstName;
-
-            set
-            {
-                if (CurrentUser?.FirstName == value)
-                    return;
-                CurrentUser.FirstName = value;
-                RaisePropertyChanged(nameof(CurrentUserFirstName));
-            }
-        }
-
-        public string CurrentUserLastName
-        {
-            get => CurrentUser?.LastName;
-
-            set
-            {
-                if (CurrentUser?.LastName == value)
-                    return;
-                CurrentUser.LastName = value;
-                RaisePropertyChanged(nameof(CurrentUserLastName));
-            }
-        }
-
-        public bool DarkModeIsEnabled
-        {
-            get => _darkModeIsEnabled;
-
-            set
-            {
-                if (_darkModeIsEnabled == value)
-                    return;
-                _darkModeIsEnabled = value;
-                RaisePropertyChanged(nameof(DarkModeIsEnabled));
-            }
-        }
-
         public ICommand ToggleDarkMode { get; }
+
+        public ViewModelBase CurrentViewModel { get; set; }
         public AdministratorViewModel AdministratorViewModel { get; set; }
         public ManagerMenuViewModel ManagerMenuViewModel { get; set; }
         public LoginViewModel LoginViewModel { get; set; }
-
-        #endregion
 
         public MainViewModel(AdministratorViewModel administratorViewModel, ManagerMenuViewModel managerMenuViewModel, LoginViewModel loginViewModel)
         {
@@ -105,7 +36,6 @@ namespace HospitalCalendar.WPF.ViewModels
             ManagerMenuViewModel = managerMenuViewModel;
             LoginViewModel = loginViewModel;
 
-
             ToggleDarkMode = new RelayCommand(ExecuteToggleDarkMode);
 
             CurrentUser = null;
@@ -114,33 +44,34 @@ namespace HospitalCalendar.WPF.ViewModels
             Logout = new RelayCommand(ExecuteLogout);
         }
 
+        private void ExecuteLogout()
+        {
+            if (DarkModeIsEnabled)
+            {
+                DarkModeIsEnabled = false;
+                ExecuteToggleDarkMode();
+            }
+
+            CurrentUser = null;
+            CurrentViewModel = LoginViewModel;
+        }
+
         private void ExecuteToggleDarkMode()
         {
             var paletteHelper = new PaletteHelper();
             //Retrieve the app's existing theme
             ITheme theme = paletteHelper.GetTheme();
 
-            theme.SetBaseTheme(DarkModeIsEnabled ? Theme.Light : Theme.Dark);
-            DarkModeIsEnabled = !DarkModeIsEnabled;
+            theme.SetBaseTheme(DarkModeIsEnabled ? Theme.Dark : Theme.Light);
 
             //Change the app's current theme
             paletteHelper.SetTheme(theme);
-        }
 
-        private void ExecuteLogout()
-        {
-            if (DarkModeIsEnabled)
-            {
-                ExecuteToggleDarkMode();
-            }
-                
-            CurrentUser = null;
-            CurrentViewModel = LoginViewModel;
+            MessengerInstance.Send(new DarkModeToggled());
         }
 
         private void ExecuteLogin( UserLoginSuccess message)
         {
-
             CurrentUser = message.User;
 
             switch (message.User)
