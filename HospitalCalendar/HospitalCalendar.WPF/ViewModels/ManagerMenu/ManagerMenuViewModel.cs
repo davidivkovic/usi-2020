054fb912df7589f543b6397276f7c718bda91541
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-18 18:57:20 +0200

    Refactored ManagerMenuViewModel on base of IL Weaving

--- HospitalCalendar/HospitalCalendar.WPF/ViewModels/ManagerMenu/ManagerMenuViewModel.cs
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/ManagerMenu/ManagerMenuViewModel.cs
@@ -8,82 +8,51 @@ using GalaSoft.MvvmLight.Ioc;
 using HospitalCalendar.Domain.Models;
 using HospitalCalendar.WPF.Messages;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu.EquipmentMenu;
+using HospitalCalendar.WPF.ViewModels.ManagerMenu.RenovationMenu;
+using HospitalCalendar.WPF.ViewModels.ManagerMenu.RoomSearchMenu;
 
 namespace HospitalCalendar.WPF.ViewModels.ManagerMenu
 {
     public class ManagerMenuViewModel : ViewModelBase
     {
-        #region Properties
-        private ViewModelBase _currentViewModel;
-
         public ICommand ShowEquipmentMenu { get; set; }
         public ICommand ShowRenovationMenu { get; set; }
         public ICommand ShowDoctorMenu { get; set; }
+        public ICommand ShowRoomSearchMenu { get; set; }
 
         public Manager Manager { get; set; }
 
-        public ViewModelBase CurrentViewModel
-        {
-            get => _currentViewModel;
+        public ViewModelBase CurrentViewModel { get; set; }
+        public EquipmentMenuViewModel EquipmentMenuViewModel { get; set; }
+        public RenovationMenuViewModel RenovationMenuViewModel { get; set; }
+        public RoomSearchViewModel RoomSearchViewModel { get; set; }
 
-            set
-            {
-                if (_currentViewModel == value)
-                    return;
-                _currentViewModel = value;
-                RaisePropertyChanged(nameof(CurrentViewModel));
 
-            }
-        }
-        #endregion
-
-        private EquipmentMenuViewModel _equipmentMenuViewModel;
-        
-        public EquipmentMenuViewModel EquipmentMenuViewModel
-        {
-            get => _equipmentMenuViewModel;
-
-            set
-            {
-                if (_equipmentMenuViewModel == value)
-                    return;
-                _equipmentMenuViewModel = value;
-                RaisePropertyChanged(nameof(EquipmentMenuViewModel));
-
-            }
-        }
-        
-        public EquipmentTypeUpdateViewModel EquipmentTypeUpdateViewModel { get; set; }
-
-        public ManagerMenuViewModel(EquipmentMenuViewModel equipmentMenuViewModel, EquipmentTypeUpdateViewModel equipmentTypeUpdateViewModel)
+        public ManagerMenuViewModel(EquipmentMenuViewModel equipmentMenuViewModel, RenovationMenuViewModel renovationMenuViewModel, RoomSearchViewModel roomSearchViewModel)
         {
             EquipmentMenuViewModel = equipmentMenuViewModel;
-            EquipmentTypeUpdateViewModel = equipmentTypeUpdateViewModel;
+            RenovationMenuViewModel = renovationMenuViewModel;
+            RoomSearchViewModel = roomSearchViewModel;
 
             ShowEquipmentMenu = new RelayCommand(ExecuteShowEquipmentMenu);
             ShowRenovationMenu = new RelayCommand(ExecuteShowRenovationMenu);
             ShowDoctorMenu = new RelayCommand(ExecuteShowDoctorMenu);
+            ShowRoomSearchMenu = new RelayCommand(ExecuteShowRoomSearchMenu);
 
             ExecuteShowEquipmentMenu();
 
             MessengerInstance.Register<CurrentUser>(this, message => Manager = message.User as Manager);
         }
 
-        private void ExecuteShowEquipmentMenu()
-        {
-            //throw new NotImplementedException();
-            CurrentViewModel = EquipmentMenuViewModel;
-        }
-
-        private void ExecuteShowRenovationMenu()
-        {
-            CurrentViewModel = EquipmentTypeUpdateViewModel;
-            //throw new NotImplementedException();
-        }
+        private void ExecuteShowRoomSearchMenu() => CurrentViewModel = RoomSearchViewModel;
+        private void ExecuteShowEquipmentMenu() => CurrentViewModel = EquipmentMenuViewModel;
+        private void ExecuteShowRenovationMenu() => CurrentViewModel = RenovationMenuViewModel;
 
         private void ExecuteShowDoctorMenu()
         {
             throw new NotImplementedException();
         }
+
+
     }
 }
