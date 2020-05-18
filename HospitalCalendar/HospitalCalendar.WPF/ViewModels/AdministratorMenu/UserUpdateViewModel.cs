671e73915dec0bb6b98de5e92f406452892b1772
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-18 18:49:06 +0200

    Refactored UserUpdateViewModel on base of IL Weaving

--- HospitalCalendar/HospitalCalendar.WPF/ViewModels/AdministratorMenu/UserUpdateViewModel.cs
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/AdministratorMenu/UserUpdateViewModel.cs
@@ -6,112 +6,56 @@ using HospitalCalendar.Domain.Models;
 using HospitalCalendar.Domain.Services;
 using HospitalCalendar.EntityFramework.Exceptions;
 using HospitalCalendar.WPF.Messages;
+using PropertyChanged;
 
 namespace HospitalCalendar.WPF.ViewModels.AdministratorMenu
 {
     public class UserUpdateViewModel : ViewModelBase
     {
-        #region Properties
-        private User _userToUpdate;
-        private string _password;
-        private string _confirmPassword;
-        private bool _usernameAlreadyExists;
-        private bool _nonMatchingPasswords;
-        
         private readonly IUserService _userService;
 
         public ICommand UpdateUser { get; set; }
-        
-        public User UserToUpdate
-        {
-            get => _userToUpdate;
-            set
-            {
-                _userToUpdate = value;
-                RaisePropertyChanged(nameof(UserToUpdate));
-                RaisePropertyChanged(nameof(FirstName));
-                RaisePropertyChanged(nameof(LastName));
-                RaisePropertyChanged(nameof(Username));
-            }
-        }
+
+        [AlsoNotifyFor(nameof(FirstName), nameof(LastName), nameof(Username))]
+        public User UserToUpdate { get; set; }
 
         public string FirstName
         {
-            get => _userToUpdate?.FirstName;
+            get => UserToUpdate?.FirstName;
             set
             {
-                if (_userToUpdate.FirstName == value) return;
-                _userToUpdate.FirstName = value;
+                if (UserToUpdate.FirstName == value) return;
+                UserToUpdate.FirstName = value;
                 RaisePropertyChanged(nameof(FirstName));
             }
         }
 
         public string LastName
         {
-            get => _userToUpdate?.LastName;
+            get => UserToUpdate?.LastName;
             set
             {
-                if (_userToUpdate.LastName == value) return;
-                _userToUpdate.LastName = value;
+                if (UserToUpdate.LastName == value) return;
+                UserToUpdate.LastName = value;
                 RaisePropertyChanged(nameof(LastName));
             }
         }
 
         public string Username
         {
-            get => _userToUpdate?.Username;
+            get => UserToUpdate?.Username;
             set
             {
-                if (_userToUpdate.Username == value) return;
-                _userToUpdate.Username = value;
+                if (UserToUpdate.Username == value) return;
+                UserToUpdate.Username = value;
                 RaisePropertyChanged(nameof(Username));
             }
         }
 
-        public string Password
-        {
-            get => _password;
-            set
-            {
-                if (_password == value) return;
-                _password = value;
-                RaisePropertyChanged(nameof(Password));
-            }
-        }
-
-        public string ConfirmPassword
-        {
-            get => _confirmPassword;
-            set
-            {
-                if (_confirmPassword == value) return;
-                _confirmPassword = value;
-                RaisePropertyChanged(nameof(ConfirmPassword));
-            }
-        }
-
-        public bool UsernameAlreadyExists
-        {
-            get => _usernameAlreadyExists;
-            set
-            {
-                if (_usernameAlreadyExists == value) return;
-                _usernameAlreadyExists = value;
-                RaisePropertyChanged(nameof(UsernameAlreadyExists));
-            }
-        }
-
-        public bool NonMatchingPasswords
-        {
-            get => _nonMatchingPasswords;
-            set
-            {
-                if (_nonMatchingPasswords == value) return;
-                _nonMatchingPasswords = value;
-                RaisePropertyChanged(nameof(NonMatchingPasswords));
-            }
-        }
-        #endregion
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public bool UsernameAlreadyExists { get; set; }
+        public bool NonMatchingPasswords { get; set; }
 
         public UserUpdateViewModel(IUserService userService)
         {
