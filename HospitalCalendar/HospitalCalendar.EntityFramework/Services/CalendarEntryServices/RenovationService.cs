ae146e803488f1402fa6757439e09edb1a3b1836
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-18 18:39:03 +0200

    Implemented Create overloads

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/CalendarEntryServices/RenovationService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/CalendarEntryServices/RenovationService.cs
@@ -14,53 +14,153 @@ namespace HospitalCalendar.EntityFramework.Services.CalendarEntryServices
         {
         }
 
-        public async Task<Renovation> Create(Room room, Room roomToAdd, RoomType newRoomType, DateTime start, DateTime end, bool splitting,
+        public async Task<Renovation> Create(Room room, Room roomToAdd, RoomType newRoomType, DateTime start, DateTime end, bool splitting, 
             ICollection<EquipmentItem> removedEquipmentItems, ICollection<EquipmentItem> addedEquipmentItems)
         {
-            Renovation entity = new Renovation()
-            {
-                Room = room,
-                RoomToAdd = roomToAdd,
+            var renovation = new Renovation()
+            {   
+                IsActive = true,
+                Room = null,
+                RoomToAdd = null,
                 NewRoomType = newRoomType,
                 StartDateTime = start,
                 EndDateTime = end,
                 Splitting = splitting,
-                RemovedEquipmentItems = removedEquipmentItems,
-                AddedEquipmentItems = addedEquipmentItems
+                RemovedEquipmentItems = null,
+                AddedEquipmentItems = null
             };
 
-            _ = await Create(entity);
-
-            return entity;
+            var createdRenovation = await Create(renovation);
+            var updatedRenovation = await Update(createdRenovation, room, roomToAdd, newRoomType, start, end, splitting, removedEquipmentItems, addedEquipmentItems);
+            return updatedRenovation;
         }
 
-        public async Task<Renovation> Update(Renovation entity,Room room, Room roomToAdd, RoomType newRoomType, DateTime start, DateTime end, bool splitting,
+        public async Task<Renovation> Create(Room room, RoomType newRoomType, DateTime start, DateTime end, ICollection<EquipmentItem> removedEquipmentItems, ICollection<EquipmentItem> addedEquipmentItems)
+        {
+            var renovation = new Renovation()
+            {
+                IsActive = true,
+                Room = null,
+                RoomToAdd = null,
+                NewRoomType = newRoomType,
+                StartDateTime = start,
+                EndDateTime = end,
+                Splitting = false,
+                RemovedEquipmentItems = null,
+                AddedEquipmentItems = null
+            };
+
+            var createdRenovation = await Create(renovation);
+            var updatedRenovation = await Update(createdRenovation, room, null, newRoomType, start, end, false, removedEquipmentItems, addedEquipmentItems);
+            return updatedRenovation;
+        }
+
+        public async Task<Renovation> Create(Room room, DateTime start, DateTime end, ICollection<EquipmentItem> removedEquipmentItems, ICollection<EquipmentItem> addedEquipmentItems)
+        {
+            var renovation = new Renovation()
+            {
+                IsActive = true,
+                Room = null,
+                RoomToAdd = null,
+                NewRoomType = room.Type,
+                StartDateTime = start,
+                EndDateTime = end,
+                Splitting = false,
+                RemovedEquipmentItems = null,
+                AddedEquipmentItems = null
+            };
+
+            var createdRenovation = await Create(renovation);
+            var updatedRenovation = await Update(createdRenovation, room, null, room.Type, start, end, false, removedEquipmentItems, addedEquipmentItems);
+            return updatedRenovation;
+        }
+
+        public async Task<Renovation> Create(Room room, DateTime start, DateTime end, bool splitting)
+        {
+            var renovation = new Renovation
+            {
+                IsActive = true,
+                Room = null,
+                RoomToAdd = null,
+                NewRoomType = room.Type,
+                StartDateTime = start,
+                EndDateTime = end,
+                Splitting = splitting,
+                RemovedEquipmentItems = null,
+                AddedEquipmentItems = null
+            };
+
+            var createdRenovation = await Create(renovation);
+            var updatedRenovation = await Update(createdRenovation, room, null, room.Type, start, end, splitting, null, null);
+            return updatedRenovation;
+        }
+
+        public async Task<Renovation> Create(Room room, Room roomToAdd, DateTime start, DateTime end)
+        {
+            var renovation = new Renovation
+            {
+                IsActive = true,
+                Room = null,
+                RoomToAdd = null,
+                NewRoomType = room.Type,
+                StartDateTime = start,
+                EndDateTime = end,
+                Splitting = false,
+                RemovedEquipmentItems = null,
+                AddedEquipmentItems = null
+            };
+
+            var createdRenovation = await Create(renovation);
+            var updatedRenovation = await Update(createdRenovation, room, roomToAdd, room.Type, start, end, false, null, null);
+            return updatedRenovation;
+        }
+
+        public async Task<Renovation> Create(Room room, DateTime start, DateTime end)
+        {
+            var renovation = new Renovation
+            {
+                IsActive = true,
+                Room = null,
+                RoomToAdd = null,
+                NewRoomType = room.Type,
+                StartDateTime = start,
+                EndDateTime = end,
+                Splitting = false,
+                RemovedEquipmentItems = null,
+                AddedEquipmentItems = null
+            };
+
+            var createdRenovation = await Create(renovation);
+            var updatedRenovation = await Update(createdRenovation, room, null, room.Type, start, end, false, null, null);
+            return updatedRenovation;
+        }
+
+        public async Task<Renovation> Update(Renovation renovation,Room room, Room roomToAdd, RoomType newRoomType, DateTime start, DateTime end, bool splitting,
             ICollection<EquipmentItem> removedEquipmentItems, ICollection<EquipmentItem> addedEquipmentItems)
         {
-            entity.Room = room;
-            entity.RoomToAdd = roomToAdd;
-            entity.NewRoomType = newRoomType;
-            entity.StartDateTime = start;
-            entity.EndDateTime = end;
-            entity.Splitting = splitting;
-            entity.RemovedEquipmentItems = removedEquipmentItems;
-            entity.AddedEquipmentItems = addedEquipmentItems;
-           
-
-            _ = await Update(entity);
-
-            return entity;
+            renovation.Room = room;
+            renovation.RoomToAdd = roomToAdd;
+            renovation.NewRoomType = newRoomType;
+            renovation.StartDateTime = start;
+            renovation.EndDateTime = end;
+            renovation.Splitting = splitting;
+            renovation.RemovedEquipmentItems = removedEquipmentItems;
+            renovation.AddedEquipmentItems = addedEquipmentItems;
+            
+            return await Update(renovation);
         }
 
-        public async Task<ICollection<Renovation>> GetAllByTimeFrame(DateTime start, DateTime end) 
+
+        public async Task<ICollection<Renovation>> GetAllByTimeFrame(DateTime start, DateTime end)
         {
-            using (HospitalCalendarDbContext context = _contextFactory.CreateDbContext()) 
-            {
-                return await context.Renovations
-                                    .Where(r => r.IsActive)
-                                    .Where(r => r.StartDateTime >= start && r.EndDateTime <= end)
-                                    .ToListAsync();
-            }
+            await using HospitalCalendarDbContext context = ContextFactory.CreateDbContext();
+            return await context.Renovations
+                .Include(r => r.Room)
+                .Where(r => r.IsActive)
+                .Where(r => (r.StartDateTime >= start && r.StartDateTime <= end) ||
+                                    (r.EndDateTime >= start && r.EndDateTime <= end) ||
+                                    (r.StartDateTime >= start && r.EndDateTime <= end))
+                .ToListAsync();
         }
     }
 }
