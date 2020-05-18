2dc70436e6540058bad7ce90c366d03e2f9175d8
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-18 18:43:16 +0200

    MainWindow can now change size

--- HospitalCalendar/HospitalCalendar.WPF/MainWindow.xaml.cs
+++ HospitalCalendar/HospitalCalendar.WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace HospitalCalendar.WPF
 {
@@ -24,8 +27,33 @@ namespace HospitalCalendar.WPF
         {
             InitializeComponent();
 
+            IsTabStopProperty.OverrideMetadata(typeof(Control), new FrameworkPropertyMetadata(false));
+            IsTabStopProperty.OverrideMetadata(typeof(TextBox), new FrameworkPropertyMetadata(true));
+
+            SettingsToggle.Focusable = false;
+
+            Loaded += (o, e) =>
+            {
+                //Height = ContentControl.ActualHeight;
+                //Width = ContentControl.ActualWidth;
+                SizeToContent = SizeToContent.WidthAndHeight;
+            };
+
             CloseButton.Click += (s, e) => Close();
             MinimizeButton.Click += (s, e) => WindowState = WindowState.Minimized;
+        }
+
+        private void SettingsToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            AnimateDarkModeMenuOpenedStoryBoard.Completed += (o, ea) =>
+            {
+                DarkModeToggle.Focus();
+            };
+        }
+
+        private void DarkModeToggle_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            SettingsToggle.IsChecked = false;
         }
     }
 }
