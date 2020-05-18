0efe9627872520ca15129d8623c691d4b4874917
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-18 18:49:22 +0200

    Refactored LoginViewModel on base of IL Weaving

--- HospitalCalendar/HospitalCalendar.WPF/ViewModels/Login/LoginViewModel.cs
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/Login/LoginViewModel.cs
@@ -19,36 +19,10 @@ namespace HospitalCalendar.WPF.ViewModels.Login
     public class LoginViewModel : ViewModelBase
     {
         private readonly IAuthenticationService _authenticationService;
-        private string _username;
-        private bool _invalidCredentials;
 
         public ICommand Login { get; }
-
-        public string Username
-        {
-            get => _username;
-
-            set
-            {
-                if (value == _username)
-                    return;
-                _username = value;
-                RaisePropertyChanged(nameof(Username));
-            }
-        }
-
-        public bool InvalidCredentials
-        {
-            get => _invalidCredentials;
-
-            set
-            {
-                if (value == _invalidCredentials)
-                    return;
-                _invalidCredentials = value;
-                RaisePropertyChanged(nameof(InvalidCredentials));
-            }
-        }
+        public string Username { get; set; }
+        public bool InvalidCredentials { get; set; }
 
         public LoginViewModel(IAuthenticationService authenticationService)
         {
