dff75e4b3ef60c87e5a95070eabce3514f777cc9
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-19 14:09:31 +0200

    Created emprty IDoctorService

---
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/DoctorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HospitalCalendar.Domain.Models;
+using HospitalCalendar.Domain.Services;
+
+namespace HospitalCalendar.EntityFramework.Services
+{
+    class DoctorService : GenericDataService<Doctor>, IDoctorService
+    {
+        public DoctorService(HospitalCalendarDbContextFactory contextFactory) : base(contextFactory)
+        {
+        }
+    }
+}
