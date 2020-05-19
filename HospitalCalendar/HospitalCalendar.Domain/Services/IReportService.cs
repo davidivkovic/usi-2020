129bee42f479903c6c1926fac88636c21a07d235
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-19 14:09:09 +0200

    Reworked IReportService

--- HospitalCalendar/HospitalCalendar.Domain/Services/IReportService.cs
+++ HospitalCalendar/HospitalCalendar.Domain/Services/IReportService.cs
@@ -5,11 +5,15 @@ using System.Threading.Tasks;
 
 namespace HospitalCalendar.Domain.Services
 {
-    public interface IReportService:IDataService<CalendarEntry>
+    public interface IReportService
     {
-        Task<bool> GenerateRoomReport(DateTime start, DateTime end);
-        Task<bool> GenerateDoctorReport(DateTime start, DateTime end);
-        Task<bool> GenerateDoctorWorkReport(Doctor doctor);
-        Task<bool> GenerateDoctorPatientReport(Doctor doctor);
+        Task<TimeSpan> TimeOccupiedByRoom(DateTime start, DateTime end, Room room);
+        Task<TimeSpan> AverageDailyOccupiedTimeByRoom(DateTime start, DateTime end, Room room);
+        Task<TimeSpan> TotalTimeOccupiedForAllRooms(DateTime start, DateTime end);
+        Task<TimeSpan> AverageDailyOccupiedTimeForAllRooms(DateTime start, DateTime end);
+        Task<TimeSpan> TimeOccupiedByDoctor(DateTime start, DateTime end, Doctor doctor);
+        Task<TimeSpan> AverageDailyOccupiedTimeByDoctor(DateTime start, DateTime end, Doctor doctor);
+        Task<TimeSpan> TotalTimeOccupiedForAllDoctors(DateTime start, DateTime end);
+        Task<TimeSpan> AverageDailyOccupiedTimeForAllDoctors(DateTime start, DateTime end);
     }
 }
