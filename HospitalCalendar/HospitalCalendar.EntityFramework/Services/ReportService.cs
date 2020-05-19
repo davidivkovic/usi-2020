a7a9cf35edf4cb503e06042d687468a17f3f61f7
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-19 14:08:57 +0200

    Reworked ReportService

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/ReportService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/ReportService.cs
@@ -5,46 +5,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System;
+using HospitalCalendar.Domain.Services.CalendarEntryServices;
 
 namespace HospitalCalendar.EntityFramework.Services
 {
-    public class ReportService:GenericDataService<CalendarEntry>,IReportService
+    public class ReportService : IReportService
     {
-        public ReportService(HospitalCalendarDbContextFactory contextFactory) : base(contextFactory)
+        private readonly ICalendarEntryService _calendarEntryService;
+        private readonly IRoomService _roomService;
+        private readonly IDoctorService _doctorService;
+        private readonly IAppointmentService _appointmentService;
+
+        public ReportService(ICalendarEntryService calendarEntryService, IAppointmentService appointmentService, IRoomService roomService, IDoctorService doctorService)
         {
+            _calendarEntryService = calendarEntryService;
+            _roomService = roomService;
+            _doctorService = doctorService;
+            _appointmentService = appointmentService;
         }
 
-        public async Task<bool> GenerateRoomReport(DateTime start, DateTime end) 
+        public async Task<TimeSpan> TimeOccupiedByRoom(DateTime start, DateTime end, Room room)
         {
-
-            List<Appointment> entrys = new List<Appointment>();
-            using (HospitalCalendarDbContext context = _contextFactory.CreateDbContext())
-            {
-                entrys =await context.Appointments
-                                .Where(ce => ce.IsActive)
-                                .ToListAsync();
-            }
-            
-
-
-            return false;
+            var calendarEntriesInTimeFrame = (await _calendarEntryService.GetAllByRoomAndTimeFrame(room, start, end)).ToList();
+            var totalTimeOccupied = calendarEntriesInTimeFrame.Aggregate(TimeSpan.Zero, (ts, ce) => ts.Add(ce.EndDateTime - ce.StartDateTime));
+            return totalTimeOccupied;
         }
 
-        public async Task<bool> GenerateDoctorReport(DateTime start, DateTime end) 
+        public async Task<TimeSpan> AverageDailyOccupiedTimeByRoom(DateTime start, DateTime end, Room room)
         {
-
-            return false;
+            var numberOfDays = (start - end).Days;
+            var totalTimeOccupied = await TimeOccupiedByRoom(start, end, room);
+            return TimeSpan.FromMinutes(totalTimeOccupied.TotalMinutes / numberOfDays);
         }
 
-        public async Task<bool> GenerateDoctorWorkReport(Doctor doctor)
+        public async Task<TimeSpan> TotalTimeOccupiedForAllRooms(DateTime start, DateTime end)
         {
-            return false;
+            var allRooms = (await _roomService.GetAll()).ToList();
+            var totalTimeOccupied = TimeSpan.Zero;
+            allRooms.ForEach(async room => totalTimeOccupied += await TimeOccupiedByRoom(start, end, room));
+            return totalTimeOccupied;
         }
 
-        public async Task<bool> GenerateDoctorPatientReport(Doctor doctor)
+        public async Task<TimeSpan> AverageDailyOccupiedTimeForAllRooms(DateTime start, DateTime end)
         {
-            return false;
+            var numberOfDays = (start - end).Days;
+            var totalTimeOccupied = await TotalTimeOccupiedForAllRooms(start, end);
+            return TimeSpan.FromMinutes(totalTimeOccupied.TotalMinutes / numberOfDays);
         }
 
+        public async Task<TimeSpan> TimeOccupiedByDoctor(DateTime start, DateTime end, Doctor doctor)
+        {
+            var appointmentsInTimeFrame = (await _appointmentService.GetAllByTimeFrame(start, end)).Where(a => a.Doctor.ID == doctor.ID).ToList();
+            var totalTimeOccupied = appointmentsInTimeFrame.Aggregate(TimeSpan.Zero, (ts, ce) => ts.Add(ce.EndDateTime - ce.StartDateTime));
+            return totalTimeOccupied;
+        }
+
+        public async Task<TimeSpan> AverageDailyOccupiedTimeByDoctor(DateTime start, DateTime end, Doctor doctor)
+        {
+            var numberOfDays = (start - end).Days;
+            var totalTimeOccupied = await TimeOccupiedByDoctor(start, end, doctor);
+            return TimeSpan.FromMinutes(totalTimeOccupied.TotalMinutes / numberOfDays);
+        }
+
+        public async Task<TimeSpan> TotalTimeOccupiedForAllDoctors(DateTime start, DateTime end)
+        {
+            var allDoctors = (await _doctorService.GetAll()).ToList();
+            var totalTimeOccupied = TimeSpan.Zero;
+            allDoctors.ForEach(async doctor => totalTimeOccupied += await TimeOccupiedByDoctor(start, end, doctor));
+            return totalTimeOccupied;
+        }
+
+        public async Task<TimeSpan> AverageDailyOccupiedTimeForAllDoctors(DateTime start, DateTime end)
+        {
+            var numberOfDays = (start - end).Days;
+            var totalTimeOccupied = await TotalTimeOccupiedForAllDoctors(start, end);
+            return TimeSpan.FromMinutes(totalTimeOccupied.TotalMinutes / numberOfDays);
+        }
     }
 }
