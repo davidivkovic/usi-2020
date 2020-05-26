f9d70bdda66f1e51555c7dad595bb2a47444e9af
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 02:31:53 +0200

    Implemented EventHandlers for selecting and unselecting Calendar Events

--- HospitalCalendar/HospitalCalendar.WPF/DataTemplates/Calendar/Control/Calendar.xaml.cs
+++ HospitalCalendar/HospitalCalendar.WPF/DataTemplates/Calendar/Control/Calendar.xaml.cs
@@ -22,5 +22,28 @@ namespace HospitalCalendar.WPF.DataTemplates.Calendar.Control
         {
             InitializeComponent();
         }
+
+        private bool _isDown;
+        private void Border_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isDown = true;
+        }
+        private void Border_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_isDown)
+            {
+                _isDown = false;
+                Keyboard.Focus(sender as Border);
+            }
+        }
+
+        private void UIElement_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            var timeLineEvent = (TimeLineEvent)((Border)sender).DataContext;
+            if (timeLineEvent.EventUnselected.CanExecute(timeLineEvent.CalendarEntry.CalendarEntry))
+            {
+                timeLineEvent.EventUnselected.Execute(timeLineEvent.CalendarEntry.CalendarEntry);
+            }
+        }
     }
 }
