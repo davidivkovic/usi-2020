47c310c896b9a4d26101e22620ef9e17c754750d
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 02:30:39 +0200

    Implemented ListToStringConverter

---
+++ HospitalCalendar/HospitalCalendar.WPF/Converters/ListToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using HospitalCalendar.Domain.Models;
+
+namespace HospitalCalendar.WPF.Converters
+{
+    public class ListToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null)
+                return value != null ? string.Join(" ", ((ICollection<Specialization>)value).Select(s => s.SingleSpecialization).ToList()) : "";
+            return value != null ? ((Specialization)value).SingleSpecialization.ToString() : "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
