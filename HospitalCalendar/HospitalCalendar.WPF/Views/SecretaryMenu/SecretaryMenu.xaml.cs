221263010ab53debbc645211bbcdca11f13f7212
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 02:44:07 +0200

    Created SecretaryMenu Views

---
+++ HospitalCalendar/HospitalCalendar.WPF/Views/SecretaryMenu/SecretaryMenu.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HospitalCalendar.WPF.Views.SecretaryMenu
+{
+    /// <summary>
+    /// Interaction logic for SecretaryMenu.xaml
+    /// </summary>
+    public partial class SecretaryMenu : UserControl
+    {
+        public SecretaryMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuToggleButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Drawer.IsLeftDrawerOpen = !Drawer.IsLeftDrawerOpen;
+        }
+    }
+}
