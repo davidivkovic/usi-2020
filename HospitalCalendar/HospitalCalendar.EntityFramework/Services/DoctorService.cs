19006b2d574b8758b22d3488821fdf1ea6ee4069
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 02:28:37 +0200

    DoctorService needs checking

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/DoctorService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/DoctorService.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using HospitalCalendar.Domain.Models;
 using HospitalCalendar.Domain.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace HospitalCalendar.EntityFramework.Services
 {
-    class DoctorService : GenericDataService<Doctor>, IDoctorService
+    public class DoctorService : GenericDataService<Doctor>, IDoctorService
     {
+        //private readonly HospitalCalendarDbContext _context;
         public DoctorService(HospitalCalendarDbContextFactory contextFactory) : base(contextFactory)
         {
+            //_context = contextFactory.CreateDbContext();
+        }
+
+        public new async Task<IEnumerable<Doctor>> GetAll()
+        {
+            //await using var context = ContextFactory.CreateDbContext();
+            return await _context.Doctors.Where(d => d.IsActive).Include(d => d.Specializations).ToListAsync();
+
+        }
+
+        public new async Task<Doctor> Get(Guid ID)
+        {
+            //await using var context = ContextFactory.CreateDbContext();
+            return await _context.Doctors.Include(d => d.Specializations).FirstOrDefaultAsync(d => d.IsActive);
         }
     }
 }
