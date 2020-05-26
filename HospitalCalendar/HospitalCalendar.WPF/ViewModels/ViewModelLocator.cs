abb43666b222842a7ac593f36522328a63f8a7c1
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 02:57:34 +0200

    Changed lifespans of services registered in the ServiceProvider

--- HospitalCalendar/HospitalCalendar.WPF/ViewModels/ViewModelLocator.cs
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/ViewModelLocator.cs
@@ -14,11 +14,14 @@ using HospitalCalendar.EntityFramework.Services.AuthenticationServices;
 using HospitalCalendar.EntityFramework.Services.CalendarEntryServices;
 using HospitalCalendar.EntityFramework.Services.EquipmentServices;
 using HospitalCalendar.WPF.ViewModels.AdministratorMenu;
+using HospitalCalendar.WPF.ViewModels.DoctorMenu;
 using HospitalCalendar.WPF.ViewModels.Login;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu;
+using HospitalCalendar.WPF.ViewModels.ManagerMenu.DoctorSpecializationsMenu;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu.EquipmentMenu;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu.RenovationMenu;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu.RoomSearchMenu;
+using HospitalCalendar.WPF.ViewModels.SecretaryMenu;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -58,21 +61,31 @@ namespace HospitalCalendar.WPF.ViewModels
             services.AddScoped<IRenovationService, RenovationService>();
             services.AddScoped<IEquipmentTypeService, EquipmentTypeService>();
             services.AddScoped<IEquipmentItemService, EquipmentItemService>();
+            services.AddScoped<IDoctorService, DoctorService>();
+            services.AddScoped<IReportService, ReportService>();
+            services.AddScoped<ISurgeryService, SurgeryService>();
+            services.AddScoped<IPatientService, PatientService>();
             services.AddScoped<HospitalCalendarDbContextFactory>();
 
             // Register ViewModels here
             services.AddScoped<MainViewModel>();
             services.AddScoped<LoginViewModel>();
+            services.AddScoped<UserRegisterViewModel>();
+            services.AddScoped<UserUpdateViewModel>();
+            services.AddScoped<RoomCreateViewModel>();
             services.AddTransient<AdministratorViewModel>();
-            services.AddTransient<UserRegisterViewModel>();
-            services.AddTransient<UserUpdateViewModel>();
-            services.AddTransient<RoomCreateViewModel>();
             services.AddTransient<ManagerMenuViewModel>();
-            services.AddTransient<EquipmentMenuViewModel>();
-            services.AddTransient<EquipmentTypeCreateViewModel>();
-            services.AddTransient<EquipmentTypeUpdateViewModel>();
-            services.AddTransient<RenovationMenuViewModel>();
-            services.AddTransient<RoomSearchViewModel>();
+            services.AddTransient<DoctorMenuViewModel>();
+            services.AddTransient<SecretaryMenuViewModel>();
+            services.AddScoped<EquipmentMenuViewModel>();
+            services.AddScoped<EquipmentTypeCreateViewModel>();
+            services.AddScoped<EquipmentTypeUpdateViewModel>();
+            services.AddScoped<RenovationMenuViewModel>();
+            services.AddScoped<RoomSearchViewModel>();
+            services.AddScoped<DoctorSpecializationsViewModel>();
+            services.AddScoped<AppointmentScheduleViewModel>();
+            services.AddScoped<AppointmentCreateViewModel>();
+            services.AddScoped<AnamnesisUpdateViewModel>();
 
             return services.BuildServiceProvider();
         }
@@ -89,5 +102,11 @@ namespace HospitalCalendar.WPF.ViewModels
         public EquipmentTypeUpdateViewModel EquipmentTypeUpdateViewModel => ServiceProvider.GetRequiredService<EquipmentTypeUpdateViewModel>();
         public RenovationMenuViewModel RenovationMenuViewModel => ServiceProvider.GetRequiredService<RenovationMenuViewModel>();
         public RoomSearchViewModel RoomSearchViewModel => ServiceProvider.GetRequiredService<RoomSearchViewModel>();
+        public DoctorSpecializationsViewModel DoctorSpecializationsViewModel => ServiceProvider.GetRequiredService<DoctorSpecializationsViewModel>();
+        public AppointmentScheduleViewModel AppointmentScheduleViewModel => ServiceProvider.GetService<AppointmentScheduleViewModel>();
+        public DoctorMenuViewModel DoctorMenuViewModel => ServiceProvider.GetService<DoctorMenuViewModel>();
+        public AppointmentCreateViewModel AppointmentCreateViewModel => ServiceProvider.GetRequiredService<AppointmentCreateViewModel>();
+        public AnamnesisUpdateViewModel AnamnesisUpdateViewModel => ServiceProvider.GetRequiredService<AnamnesisUpdateViewModel>();
+        public SecretaryMenuViewModel SecretaryMenuViewModel => ServiceProvider.GetRequiredService<SecretaryMenuViewModel>();
     }
 }
