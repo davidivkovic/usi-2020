2bba09b7ee876f4e4307bc71d25e9ac5e8f5274f
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 02:27:03 +0200

    Created IPatientService

---
+++ HospitalCalendar/HospitalCalendar.Domain/Services/IPatientService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalCalendar.Domain.Models;
+
+namespace HospitalCalendar.Domain.Services
+{
+    public interface IPatientService : IDataService<Patient>
+    {
+        new Task<Patient> Get(Guid ID);
+    }
+}
