c9ef2b69906d700fa98c841aa541e206173f3615
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 02:41:27 +0200

    Created AnamnesisUpdateViewModel

---
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/DoctorMenu/AnamnesisUpdateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace HospitalCalendar.WPF.ViewModels.DoctorMenu
+{
+    public class AnamnesisUpdateViewModel : ViewModelBase
+    {
+    }
+}
