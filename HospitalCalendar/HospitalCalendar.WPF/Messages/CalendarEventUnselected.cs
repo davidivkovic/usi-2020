0142f6c04c64d44e8d45af313a3a173a3a341c7d
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 02:33:23 +0200

    Implemented messages for Calendar Events

---
+++ HospitalCalendar/HospitalCalendar.WPF/Messages/CalendarEventUnselected.cs
@@ -0,0 +1,17 @@
+﻿using HospitalCalendar.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalCalendar.WPF.Messages
+{
+    public class CalendarEventUnselected
+    {
+        public CalendarEntry CalendarEntry { get; set; }
+
+        public CalendarEventUnselected(CalendarEntry calendarEntry)
+        {
+            CalendarEntry = calendarEntry;
+        }
+    }
+}
