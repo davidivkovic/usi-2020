59f024e920d7ed99d3706f83462357984611bb27
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 03:02:34 +0200

    Implemented the DoctorMenu View

---
+++ HospitalCalendar/HospitalCalendar.WPF/Views/DoctorMenu/DoctorMenu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HospitalCalendar.WPF.ViewModels.DoctorMenu;
+
+namespace HospitalCalendar.WPF.Views.DoctorMenu
+{
+    /// <summary>
+    /// Interaction logic for DoctorMenu.xaml
+    /// </summary>
+    public partial class DoctorMenu : UserControl
+    {
+        public DoctorMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuToggleButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Drawer.IsLeftDrawerOpen = !Drawer.IsLeftDrawerOpen;
+        }
+    }
+}
