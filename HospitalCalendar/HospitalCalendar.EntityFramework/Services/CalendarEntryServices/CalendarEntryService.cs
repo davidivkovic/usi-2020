4704fa24527f642f656938b76c6646d74ca87060
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-05-26 02:27:52 +0200

    Reworked CalendarEntryService

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/CalendarEntryServices/CalendarEntryService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/CalendarEntryServices/CalendarEntryService.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HospitalCalendar.Domain.Models;
+using HospitalCalendar.Domain.Services;
 using HospitalCalendar.Domain.Services.CalendarEntryServices;
+using Microsoft.EntityFrameworkCore;
 
 namespace HospitalCalendar.EntityFramework.Services.CalendarEntryServices
 {
@@ -13,18 +15,23 @@ namespace HospitalCalendar.EntityFramework.Services.CalendarEntryServices
     {
         private readonly IRenovationService _renovationService;
         private readonly IAppointmentService _appointmentService;
-        public CalendarEntryService(HospitalCalendarDbContextFactory contextFactory, IRenovationService renovationService, IAppointmentService appointmentService) : base(contextFactory)
+        private readonly ISurgeryService _surgeryService;
+
+        public CalendarEntryService(HospitalCalendarDbContextFactory contextFactory, IRenovationService renovationService,
+            IAppointmentService appointmentService, ISurgeryService surgeryService) : base(contextFactory)
         {
             _renovationService = renovationService;
             _appointmentService = appointmentService;
+            _surgeryService = surgeryService;
         }
 
         public async Task<ICollection<CalendarEntry>> GetAllByTimeFrame(DateTime start, DateTime end)
         {
             var calendarEntries = new List<CalendarEntry>();
-            
+
             calendarEntries.AddRange((await _renovationService.GetAllByTimeFrame(start, end)).ToList());
             calendarEntries.AddRange((await _appointmentService.GetAllByTimeFrame(start, end)).ToList());
+            calendarEntries.AddRange((await _surgeryService.GetAllByTimeFrame(start, end)).ToList());
 
             return calendarEntries;
         }
@@ -32,8 +39,28 @@ namespace HospitalCalendar.EntityFramework.Services.CalendarEntryServices
 
         public async Task<ICollection<CalendarEntry>> GetAllByRoomAndTimeFrame(Room room, DateTime start, DateTime end)
         {
-            return (await GetAllByTimeFrame(start, end))
-                .Where(ce => ce.Room.Floor == room.Floor && ce.Room.Number == room.Number)
+            var entries = await GetAllByTimeFrame(start, end);
+            return entries.Where(ce => ce.Room.Floor == room.Floor && ce.Room.Number == room.Number).ToList();
+        }
+
+
+        public async Task<ICollection<CalendarEntry>> GetAllByDoctor(Doctor doctor)
+        {
+            var calendarEntries = new List<CalendarEntry>();
+
+            calendarEntries.AddRange((await _surgeryService.GetAllByDoctor(doctor)).ToList());
+            calendarEntries.AddRange((await _appointmentService.GetAllByDoctor(doctor)).ToList());
+
+            return calendarEntries;
+        }
+
+
+        public async Task<ICollection<CalendarEntry>> GetAllByDoctorAndTimeFrame(Doctor doctor, DateTime start, DateTime end)
+        {
+            return (await GetAllByDoctor(doctor))
+                .Where(a => (a.StartDateTime >= start && a.StartDateTime <= end) ||
+                            (a.EndDateTime >= start && a.EndDateTime <= end) ||
+                            (a.StartDateTime >= start && a.EndDateTime <= end))
                 .ToList();
         }
     }
