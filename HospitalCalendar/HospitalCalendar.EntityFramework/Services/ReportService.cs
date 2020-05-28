2434e450bf9c3a95b4e4da102557eea3c0f89c59
Milan Sekulic <sekulic.sw54.2019@uns.ac.rs>
2020-05-28 16:42:00 +0200

    Completed base versions of report generators

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/ReportService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/ReportService.cs
@@ -2,10 +2,12 @@
 using HospitalCalendar.Domain.Services;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System;
 using HospitalCalendar.Domain.Services.CalendarEntryServices;
+using IronPdf;
 
 namespace HospitalCalendar.EntityFramework.Services
 {
@@ -80,6 +82,46 @@ namespace HospitalCalendar.EntityFramework.Services
             var numberOfDays = (start - end).Days;
             var totalTimeOccupied = await TotalTimeOccupiedForAllDoctors(start, end);
             return TimeSpan.FromMinutes(totalTimeOccupied.TotalMinutes / numberOfDays);
+        }
+
+        public async Task GenerateRoomReport(DateTime start, DateTime end)
+        {
+            var rowTemplate = "<ul><li>[[FLOOR]]</li><li>[[NUMBER]]</li><li>[[PURPOSE]]</li><li>[[TOTAL]]</li><li>[[AVERAGE]]</li></ul> ";
+            var outputHTML = "";
+            var allRooms = (await _roomService.GetAll()).ToList();
+            foreach (var room in allRooms)
+            {
+                var totalTime = await TimeOccupiedByRoom(start, end, room);
+                var averageTime = await AverageDailyOccupiedTimeByRoom(start, end, room);
+                outputHTML += rowTemplate.Replace("[[FLOOR]]", room.Floor.ToString()).Replace("[[NUMBER]]", room.Number.ToString()).Replace("[[PURPOSE]]", room.GetType().ToString()).Replace("[[TOTAL]]", new DateTime(totalTime.Ticks).ToString("HH:mm")).Replace("[[AVERAGE]]", new DateTime(averageTime.Ticks).ToString("HH:mm"));
+            }
+            var html = File.ReadAllText("room.html");
+            var totalTimeAll = await TotalTimeOccupiedForAllRooms(start, end);
+            var averageTimeAll = await AverageDailyOccupiedTimeForAllRooms(start, end);
+            html = html.Replace("[[STARTDATE]]", start.ToString("yyyy/MM/dd")).Replace("[[ENDDATE]]", end.ToString("yyyy/MM/dd")).Replace("[[TOTAL]]", new DateTime(totalTimeAll.Ticks).ToString()).Replace("[[AVERAGE_OVERALL]]", new DateTime(averageTimeAll.Ticks).ToString()).Replace("<!--CONTENT-->", outputHTML);
+            var Renderer = new IronPdf.HtmlToPdf();
+            var PDF = Renderer.RenderHtmlAsPdf(html);
+            PDF.SaveAs("RoomReport.pdf");
+        }
+
+        public async Task GenerateDoctorReport(DateTime start, DateTime end)
+        {
+            var rowTemplate = "<ul><li>[[NAME]]</li><li>[[LNAME]]</li><li>[[TOTAL]]</li><li>[[AVERAGE]]</li></ul> ";
+            var outputHTML = "";
+            var allDoctors = (await _doctorService.GetAll()).ToList();
+            foreach (var doctor in allDoctors)
+            {
+                var totalTime = await TimeOccupiedByDoctor(start, end, doctor);
+                var averageTime = await TimeOccupiedByDoctor(start, end, doctor);
+                outputHTML += rowTemplate.Replace("[[NAME]]", doctor.FirstName).Replace("[[LNAME]]", doctor.LastName).Replace("[[TOTAL]]", new DateTime(totalTime.Ticks).ToString()).Replace("[[AVERAGE]]", new DateTime(averageTime.Ticks).ToString());
+            }
+            var html = File.ReadAllText("doctors.html");
+            var totalTimeAll = await TotalTimeOccupiedForAllDoctors(start, end);
+            var averageTimeAll = await AverageDailyOccupiedTimeForAllDoctors(start, end);
+            html = html.Replace("[[STARTDATE]]", start.ToString("yyyy/MM/dd")).Replace("[[ENDDATE]]", end.ToString("yyyy/MM/dd")).Replace("[[TOTAL]]", new DateTime(totalTimeAll.Ticks).ToString()).Replace("[[AVERAGE_OVERALL]]", new DateTime(averageTimeAll.Ticks).ToString()).Replace("<!--CONTENT-->", outputHTML);
+            var Renderer = new IronPdf.HtmlToPdf();
+            var PDF = Renderer.RenderHtmlAsPdf(html);
+            PDF.SaveAs("DoctorReport.pdf");
         }
     }
 }
