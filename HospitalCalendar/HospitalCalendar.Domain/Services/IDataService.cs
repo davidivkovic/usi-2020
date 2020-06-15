dd35d1ceb3332c372017e7f7557f05ae0340eb8a
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 16:29:14 +0200

    Update IDataService.cs

--- HospitalCalendar/HospitalCalendar.Domain/Services/IDataService.cs
+++ HospitalCalendar/HospitalCalendar.Domain/Services/IDataService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace HospitalCalendar.Domain.Services
@@ -7,13 +8,9 @@ namespace HospitalCalendar.Domain.Services
     public interface IDataService<T>
     {
         Task<IEnumerable<T>> GetAll();
-
-        Task<T> Get(Guid ID);
-
+        Task<T> Get(Guid id);
         Task<T> Create(T entity);
-
         Task<T> Update(T entity);
-
-        Task<bool> Delete(Guid ID);
+        Task<bool> Delete(Guid id);
     }
 }
