bb9b421d90572ff40b9e7ee15568dca04d87a83f
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 16:28:29 +0200

    Extended IEquipmentItemService

--- HospitalCalendar/HospitalCalendar.Domain/Services/EquipmentServices/IEquipmentItemService.cs
+++ HospitalCalendar/HospitalCalendar.Domain/Services/EquipmentServices/IEquipmentItemService.cs
@@ -1,6 +1,5 @@
 using HospitalCalendar.Domain.Models;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace HospitalCalendar.Domain.Services.EquipmentServices
@@ -10,10 +9,14 @@ namespace HospitalCalendar.Domain.Services.EquipmentServices
         Task<ICollection<EquipmentItem>> GetAllByType(EquipmentType equipmentType);
         Task<ICollection<EquipmentItem>> GetAllFreeByType(EquipmentType equipmentType);
         Task<ICollection<EquipmentItem>> GetAllInUseByType(EquipmentType equipmentType);
+        Task<ICollection<EquipmentItem>> GetAllByTypeInRoom(EquipmentType equipmentType, Room room);
+        Task<int> CountByType(EquipmentType equipmentType);
+        Task<int> CountFreeByType(EquipmentType equipmentType);
+        Task<int> CountInUseByType(EquipmentType equipmentType);
+        Task<int> CountByTypeInRoom(EquipmentType equipmentType, Room room);
         Task<ICollection<EquipmentItem>> GetAllWithoutRoom();
         Task<ICollection<EquipmentItem>> GetAllByRoom(Room room);
         Task<bool> Create(EquipmentType equipmentType, int count);
         Task<bool> Remove(EquipmentType equipmentType, int amount);
-        Task<ICollection<EquipmentItem>> GetAllByTypeInRoom(EquipmentType equipmentType, Room room);
     }
 }
