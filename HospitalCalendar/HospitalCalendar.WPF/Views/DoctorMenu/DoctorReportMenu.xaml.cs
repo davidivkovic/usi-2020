efbcb53be46bf7826f76c10633b0588307d877cd
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 19:01:42 +0200

    Started working on the DoctorsReportMenu

---
+++ HospitalCalendar/HospitalCalendar.WPF/Views/DoctorMenu/DoctorReportMenu.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HospitalCalendar.WPF.ViewModels.ManagerMenu.ReportMenu;
+using MaterialDesignExtensions.Controls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HospitalCalendar.WPF.Views.DoctorMenu
+{
+    /// <summary>
+    /// Interaction logic for ManagerReportMenu.xaml
+    /// </summary>
+    public partial class DoctorReportMenu : UserControl
+    {
+        public DoctorReportMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FileSystemControl_OnCancel(object sender, RoutedEventArgs e)
+        {
+            MdDialog.IsOpen = false;
+        }
+
+        private void OpenDirectoryControl_OnDirectorySelected(object sender, RoutedEventArgs e)
+        {
+            var viewModel = (ManagerReportMenuViewModel)DataContext;
+            MdDialog.IsOpen = false;
+            viewModel.FilePath = ((OpenDirectoryControl)sender).CurrentDirectory;
+        }
+    }
+}
