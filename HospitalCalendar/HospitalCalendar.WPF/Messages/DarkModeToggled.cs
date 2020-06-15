ad2e8dba54efda3e9fcd4e8b803c8f2fea28ceca
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 18:54:10 +0200

    Update DarkModeToggled.cs

--- HospitalCalendar/HospitalCalendar.WPF/Messages/DarkModeToggled.cs
+++ HospitalCalendar/HospitalCalendar.WPF/Messages/DarkModeToggled.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace HospitalCalendar.WPF.Messages
+﻿namespace HospitalCalendar.WPF.Messages
 {
     public class DarkModeToggled
     {
