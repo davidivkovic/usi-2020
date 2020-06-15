6a9ab1a6a528b2a57f3032cc33dacc00447d92e6
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 16:25:07 +0200

    Created a FileFormat enum for reports

---
+++ HospitalCalendar/HospitalCalendar.WPF/Converters/BoolToInverseConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace HospitalCalendar.WPF.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class BoolToInverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}
---
+++ HospitalCalendar/HospitalCalendar.WPF/Views/ManagerMenu/ReportMenu/ManagerReportMenu.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HospitalCalendar.WPF.ViewModels.ManagerMenu.ReportMenu;
+using MaterialDesignExtensions.Controls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HospitalCalendar.WPF.Views.ManagerMenu.ReportMenu
+{
+    /// <summary>
+    /// Interaction logic for ManagerReportMenu.xaml
+    /// </summary>
+    public partial class ManagerReportMenu : UserControl
+    {
+        public ManagerReportMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FileSystemControl_OnCancel(object sender, RoutedEventArgs e)
+        {
+            MdDialog.IsOpen = false;
+        }
+
+        private void OpenDirectoryControl_OnDirectorySelected(object sender, RoutedEventArgs e)
+        {
+            var viewModel = (ManagerReportMenuViewModel)DataContext;
+            MdDialog.IsOpen = false;
+            viewModel.FilePath = ((OpenDirectoryControl)sender).CurrentDirectory;
+        }
+    }
+}
