544997afd9ae0cd14f1bc947037fa9e71cf6aa41
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 16:42:17 +0200

    Update CalendarEntryService.cs

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/CalendarEntryServices/CalendarEntryService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/CalendarEntryServices/CalendarEntryService.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections;
+﻿using HospitalCalendar.Domain.Models;
+using HospitalCalendar.Domain.Services.CalendarEntryServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using HospitalCalendar.Domain.Models;
-using HospitalCalendar.Domain.Services;
-using HospitalCalendar.Domain.Services.CalendarEntryServices;
-using Microsoft.EntityFrameworkCore;
 
 namespace HospitalCalendar.EntityFramework.Services.CalendarEntryServices
 {
@@ -25,6 +21,36 @@ namespace HospitalCalendar.EntityFramework.Services.CalendarEntryServices
             _surgeryService = surgeryService;
         }
 
+        public async Task<CalendarEntry> CreateAppointment(DateTime start, DateTime end, Doctor doctor, Patient patient, Room room)
+        {
+            return await _appointmentService.Create(start, end, doctor, patient, room);
+        }
+
+        public async Task<AppointmentRequest> CreateAppointmentRequest(DateTime start, DateTime end, Patient patient, Doctor requester, Doctor proposedDoctor, DateTime timestamp, Room room)
+        {
+            return await _appointmentService.CreateAppointmentRequest(start, end, patient, requester, proposedDoctor, timestamp, room);
+        }
+
+        public async Task<AppointmentRequest> CreateSurgeryRequest(DateTime start, DateTime end, Patient patient, Doctor requester, Doctor proposedDoctor, bool isUrgent, DateTime timestamp, Room room)
+        {
+            return await _surgeryService.CreateSurgeryRequest(start, end, patient, requester, proposedDoctor, isUrgent, timestamp, room);
+        }
+
+        public async Task<CalendarEntry> CreateSurgery(DateTime start, DateTime end, Doctor doctor, Patient patient, Room room, bool isUrgent)
+        {
+            return await _surgeryService.Create(start, end, doctor, patient, room, isUrgent);
+        }
+
+        public async Task<ICollection<Appointment>> GetAllByPatient(Patient patient)
+        {
+            var appointments = new List<Appointment>();
+
+            appointments.AddRange((await _appointmentService.GetAllByPatient(patient)).ToList());
+            appointments.AddRange((await _surgeryService.GetAllByPatient(patient)).ToList());
+
+            return appointments;
+        }
+
         public async Task<ICollection<CalendarEntry>> GetAllByTimeFrame(DateTime start, DateTime end)
         {
             var calendarEntries = new List<CalendarEntry>();
@@ -36,32 +62,45 @@ namespace HospitalCalendar.EntityFramework.Services.CalendarEntryServices
             return calendarEntries;
         }
 
-
         public async Task<ICollection<CalendarEntry>> GetAllByRoomAndTimeFrame(Room room, DateTime start, DateTime end)
         {
+            // ToList
             var entries = await GetAllByTimeFrame(start, end);
-            return entries.Where(ce => ce.Room.Floor == room.Floor && ce.Room.Number == room.Number).ToList();
+            if(entries.Any())
+                return entries.Where(calendarEntry => calendarEntry.Room.Floor == room.Floor && calendarEntry.Room.Number == room.Number).ToList();
+            return new List<CalendarEntry>();
         }
 
-
-        public async Task<ICollection<CalendarEntry>> GetAllByDoctor(Doctor doctor)
+        public async Task<ICollection<Appointment>> GetAllByDoctor(Doctor doctor)
         {
-            var calendarEntries = new List<CalendarEntry>();
+            var appointments = new List<Appointment>();
 
-            calendarEntries.AddRange((await _surgeryService.GetAllByDoctor(doctor)).ToList());
-            calendarEntries.AddRange((await _appointmentService.GetAllByDoctor(doctor)).ToList());
+            appointments.AddRange((await _surgeryService.GetAllByDoctor(doctor)).ToList());
+            appointments.AddRange((await _appointmentService.GetAllByDoctor(doctor)).ToList());
 
-            return calendarEntries;
+            return appointments;
         }
 
-
-        public async Task<ICollection<CalendarEntry>> GetAllByDoctorAndTimeFrame(Doctor doctor, DateTime start, DateTime end)
+        public async Task<ICollection<Appointment>> GetAllByDoctorAndTimeFrame(Doctor doctor, DateTime start, DateTime end)
         {
             return (await GetAllByDoctor(doctor))
-                .Where(a => (a.StartDateTime >= start && a.StartDateTime <= end) ||
-                            (a.EndDateTime >= start && a.EndDateTime <= end) ||
-                            (a.StartDateTime >= start && a.EndDateTime <= end))
+                .Where(calendarEntry => (calendarEntry.StartDateTime >= start && calendarEntry.StartDateTime <= end) ||
+                            (calendarEntry.EndDateTime >= start && calendarEntry.EndDateTime <= end) ||
+                            (calendarEntry.StartDateTime >= start && calendarEntry.EndDateTime <= end))
                 .ToList();
+        }
+
+        // Was this task successful?
+        public async Task<bool> CancelCalendarEntry(CalendarEntry calendarEntry)
+        {
+            if (calendarEntry.Status == AppointmentStatus.Finished || calendarEntry.Status == AppointmentStatus.InProgress)
+            {
+                return false;
+            }
+
+            calendarEntry.Status = AppointmentStatus.Cancelled;
+            await Update(calendarEntry);
+            return true;
         }
     }
 }
