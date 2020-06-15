854f7bd10691f981ddea974c6139aee9c0fe4d1f
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 18:57:06 +0200

    Refactored UserUpdateViewModel

--- HospitalCalendar/HospitalCalendar.WPF/ViewModels/AdministratorMenu/UserUpdateViewModel.cs
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/AdministratorMenu/UserUpdateViewModel.cs
@@ -1,58 +1,24 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using GalaSoft.MvvmLight;
+﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using HospitalCalendar.Domain.Models;
 using HospitalCalendar.Domain.Services;
 using HospitalCalendar.EntityFramework.Exceptions;
 using HospitalCalendar.WPF.Messages;
 using PropertyChanged;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using HospitalCalendar.Domain.Services.UserServices;
 
 namespace HospitalCalendar.WPF.ViewModels.AdministratorMenu
 {
     public class UserUpdateViewModel : ViewModelBase
     {
         private readonly IUserService _userService;
-
         public ICommand UpdateUser { get; set; }
-
-        [AlsoNotifyFor(nameof(FirstName), nameof(LastName), nameof(Username))]
         public User UserToUpdate { get; set; }
-
-        public string FirstName
-        {
-            get => UserToUpdate?.FirstName;
-            set
-            {
-                if (UserToUpdate.FirstName == value) return;
-                UserToUpdate.FirstName = value;
-                RaisePropertyChanged(nameof(FirstName));
-            }
-        }
-
-        public string LastName
-        {
-            get => UserToUpdate?.LastName;
-            set
-            {
-                if (UserToUpdate.LastName == value) return;
-                UserToUpdate.LastName = value;
-                RaisePropertyChanged(nameof(LastName));
-            }
-        }
-
-        public string Username
-        {
-            get => UserToUpdate?.Username;
-            set
-            {
-                if (UserToUpdate.Username == value) return;
-                UserToUpdate.Username = value;
-                RaisePropertyChanged(nameof(Username));
-            }
-        }
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
         public bool UsernameAlreadyExists { get; set; }
@@ -65,10 +31,13 @@ namespace HospitalCalendar.WPF.ViewModels.AdministratorMenu
             MessengerInstance.Register<UserUpdateRequest>(this, message =>
             {
                 UserToUpdate = message.User;
+                FirstName = UserToUpdate.FirstName;
+                LastName = UserToUpdate.LastName;
+                Username = UserToUpdate.Username;
             });
         }
 
-        private void ExecuteUpdateUser()
+        private async void ExecuteUpdateUser()
         {
             UsernameAlreadyExists = false;
             NonMatchingPasswords = false;
@@ -79,19 +48,16 @@ namespace HospitalCalendar.WPF.ViewModels.AdministratorMenu
                 return;
             }
 
-            Task.Run(async() =>
+            try
             {
-                try
-                {
-                    var userToUpdate = await _userService.Get(UserToUpdate.ID);
-                    var updatedUser = _userService.Update(userToUpdate, FirstName, LastName, Username, Password).GetAwaiter().GetResult();
-                    MessengerInstance.Send(new UserUpdateSuccess(updatedUser));
-                }
-                catch (UsernameAlreadyExistsException)
-                {
-                    UsernameAlreadyExists = true;
-                }
-            });
+                var userToUpdate = await _userService.Get(UserToUpdate.ID);
+                var updatedUser = await _userService.Update(userToUpdate, FirstName, LastName, Username, Password);
+                MessengerInstance.Send(new UserUpdateSuccess(updatedUser));
+            }
+            catch (UsernameAlreadyExistsException)
+            {
+                UsernameAlreadyExists = true;
+            }
         }
     }
 }
