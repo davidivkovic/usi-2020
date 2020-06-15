9297a2e4dec84540bc9180e4ff96618cb92d7253
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 18:57:43 +0200

    Update LoginViewModel.cs

--- HospitalCalendar/HospitalCalendar.WPF/ViewModels/Login/LoginViewModel.cs
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/Login/LoginViewModel.cs
@@ -1,37 +1,31 @@
-﻿
-using System;
-using System.ComponentModel;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Input;
-using GalaSoft.MvvmLight;
+﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using GalaSoft.MvvmLight.Messaging;
 using HospitalCalendar.Domain.Exceptions;
-using HospitalCalendar.Domain.Models;
 using HospitalCalendar.Domain.Services.AuthenticationServices;
 using HospitalCalendar.EntityFramework.Exceptions.HospitalCalendar.Domain.Exceptions;
 using HospitalCalendar.WPF.Messages;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace HospitalCalendar.WPF.ViewModels.Login
 {
     public class LoginViewModel : ViewModelBase
     {
         private readonly IAuthenticationService _authenticationService;
-
         public ICommand Login { get; }
         public string Username { get; set; }
         public bool InvalidCredentials { get; set; }
+        public bool IsBusy { get; set; }
 
         public LoginViewModel(IAuthenticationService authenticationService)
         {
             _authenticationService = authenticationService;
-            Login = new RelayCommand<PasswordBox>(async(passwordBox) => await ExecuteLogin(passwordBox.Password));
+            Login = new RelayCommand<PasswordBox>(passwordBox => ExecuteLogin(passwordBox.Password));
         }
 
-        private async Task ExecuteLogin(string password)
+        private async void ExecuteLogin(string password)
         {
+            IsBusy = true;
             InvalidCredentials = false;
             try
             {
@@ -45,6 +39,10 @@ namespace HospitalCalendar.WPF.ViewModels.Login
             catch (InvalidPasswordException)
             {
                 InvalidCredentials = true;
+            }
+            finally
+            {
+                IsBusy = false;
             }
         }
     }
