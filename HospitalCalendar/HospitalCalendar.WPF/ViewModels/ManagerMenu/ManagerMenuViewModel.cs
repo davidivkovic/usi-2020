62d70e85c62724aa33558b7803c1f9eb0654bc8b
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 18:59:49 +0200

    Update ManagerMenuViewModel.cs

--- HospitalCalendar/HospitalCalendar.WPF/ViewModels/ManagerMenu/ManagerMenuViewModel.cs
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/ManagerMenu/ManagerMenuViewModel.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Input;
-using GalaSoft.MvvmLight;
+﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using GalaSoft.MvvmLight.Ioc;
 using HospitalCalendar.Domain.Models;
 using HospitalCalendar.WPF.Messages;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu.DoctorSpecializationsMenu;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu.EquipmentMenu;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu.RenovationMenu;
+using HospitalCalendar.WPF.ViewModels.ManagerMenu.ReportMenu;
 using HospitalCalendar.WPF.ViewModels.ManagerMenu.RoomSearchMenu;
+using System.Windows.Input;
 
 namespace HospitalCalendar.WPF.ViewModels.ManagerMenu
 {
@@ -20,6 +17,7 @@ namespace HospitalCalendar.WPF.ViewModels.ManagerMenu
         public ICommand ShowRenovationMenu { get; set; }
         public ICommand ShowDoctorMenu { get; set; }
         public ICommand ShowRoomSearchMenu { get; set; }
+        public ICommand ShowReportMenu { get; set; }
 
         public Manager Manager { get; set; }
 
@@ -28,12 +26,14 @@ namespace HospitalCalendar.WPF.ViewModels.ManagerMenu
         public RenovationMenuViewModel RenovationMenuViewModel { get; set; }
         public RoomSearchViewModel RoomSearchViewModel { get; set; }
         public DoctorSpecializationsViewModel DoctorSpecializationsViewModel { get; set; }
+        public ManagerReportMenuViewModel ManagerReportMenuViewModel { get; set; }
 
-        public ManagerMenuViewModel(EquipmentMenuViewModel equipmentMenuViewModel, RenovationMenuViewModel renovationMenuViewModel, 
+        public ManagerMenuViewModel(EquipmentMenuViewModel equipmentMenuViewModel, RenovationMenuViewModel renovationMenuViewModel, ManagerReportMenuViewModel managerReportMenuViewModel,
                                     RoomSearchViewModel roomSearchViewModel, DoctorSpecializationsViewModel doctorSpecializationsViewModel)
         {
             EquipmentMenuViewModel = equipmentMenuViewModel;
             RenovationMenuViewModel = renovationMenuViewModel;
+            ManagerReportMenuViewModel = managerReportMenuViewModel;
             RoomSearchViewModel = roomSearchViewModel;
             DoctorSpecializationsViewModel = doctorSpecializationsViewModel;
 
@@ -42,8 +42,16 @@ namespace HospitalCalendar.WPF.ViewModels.ManagerMenu
             ShowRenovationMenu = new RelayCommand(ExecuteShowRenovationMenu);
             ShowDoctorMenu = new RelayCommand(ExecuteShowDoctorMenu);
             ShowRoomSearchMenu = new RelayCommand(ExecuteShowRoomSearchMenu);
+            ShowReportMenu = new RelayCommand(ExecuteShowReportMenu);
 
             ExecuteShowRenovationMenu();
+        }
+
+        private void ExecuteShowReportMenu()
+        {
+            if (CurrentViewModel == ManagerReportMenuViewModel) return;
+            ManagerReportMenuViewModel.Initialize();
+            CurrentViewModel = ManagerReportMenuViewModel;
         }
 
         private void ExecuteShowRoomSearchMenu()
