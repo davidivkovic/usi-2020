527a6b7aa9e91c319f0ecc89275784b7a735e685
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 16:28:33 +0200

    Create IAnamnesisService.cs

---
+++ HospitalCalendar/HospitalCalendar.Domain/Services/IAnamnesisService.cs
@@ -0,0 +1,13 @@
+﻿using HospitalCalendar.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HospitalCalendar.Domain.Services
+{
+    public interface IAnamnesisService : IDataService<Entry>
+    {
+        Task<Entry> AddEntry(Appointment appointment, string entry, DateTime timestamp);
+        Task<ICollection<Entry>> GetAllByPatient(Patient patient);
+    }
+}
