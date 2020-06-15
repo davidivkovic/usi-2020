00b43097c19eba30ebb3f25abc6a576114e705e8
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 18:57:31 +0200

    Create DoctorReportMenuViewModel.cs

---
+++ HospitalCalendar/HospitalCalendar.WPF/ViewModels/DoctorMenu/DoctorReportMenuViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace HospitalCalendar.WPF.ViewModels.DoctorMenu
+{
+    public class DoctorReportMenuViewModel : ViewModelBase
+    {
+
+    }
+}
