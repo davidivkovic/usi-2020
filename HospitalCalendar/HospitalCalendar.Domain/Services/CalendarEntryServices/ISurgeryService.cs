8b0d3e7a43abf1233322b4f16e71d40aae1d597e
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 16:28:12 +0200

    Create ISurgeryService.cs

---
+++ HospitalCalendar/HospitalCalendar.Domain/Services/CalendarEntryServices/ISurgeryService.cs
@@ -0,0 +1,17 @@
+﻿using HospitalCalendar.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HospitalCalendar.Domain.Services.CalendarEntryServices
+{
+    public interface ISurgeryService : IDataService<Surgery>
+    {
+        Task<ICollection<Surgery>> GetAllByTimeFrame(DateTime start, DateTime end);
+        Task<ICollection<Surgery>> GetAllByDoctor(Doctor doctor);
+        Task<Surgery> Create(DateTime start, DateTime end, Doctor doctor, Patient patient, Room room, bool isUrgent);
+        Task<ICollection<Surgery>> GetAllByPatient(Patient patient);
+        Task<AppointmentRequest> CreateSurgeryRequest(DateTime start, DateTime end, Patient patient, Doctor requester, Doctor proposedDoctor, bool isUrgent, DateTime timestamp, Room room);
+    }
+}
+    
