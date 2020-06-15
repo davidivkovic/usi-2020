5b6a42664805569dd6436162e3b0e5d1e38d8ee0
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-15 16:27:52 +0200

    Extended ICalendarEntryService

--- HospitalCalendar/HospitalCalendar.Domain/Services/CalendarEntryServices/ICalendarEntryService.cs
+++ HospitalCalendar/HospitalCalendar.Domain/Services/CalendarEntryServices/ICalendarEntryService.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using HospitalCalendar.Domain.Models;
+using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
 using System.Threading.Tasks;
-using HospitalCalendar.Domain.Models;
 
 namespace HospitalCalendar.Domain.Services.CalendarEntryServices
 {
@@ -11,7 +9,13 @@ namespace HospitalCalendar.Domain.Services.CalendarEntryServices
     {
         Task<ICollection<CalendarEntry>> GetAllByTimeFrame(DateTime start, DateTime end);
         Task<ICollection<CalendarEntry>> GetAllByRoomAndTimeFrame(Room room, DateTime start, DateTime end);
-        Task<ICollection<CalendarEntry>> GetAllByDoctor(Doctor doctor);
-        Task<ICollection<CalendarEntry>> GetAllByDoctorAndTimeFrame(Doctor doctor, DateTime start, DateTime end);
+        Task<ICollection<Appointment>> GetAllByDoctor(Doctor doctor);
+        Task<ICollection<Appointment>> GetAllByDoctorAndTimeFrame(Doctor doctor, DateTime start, DateTime end);
+        Task<ICollection<Appointment>> GetAllByPatient(Patient patient);
+        Task<CalendarEntry> CreateAppointment(DateTime start, DateTime end, Doctor doctor, Patient patient, Room room);
+        Task<CalendarEntry> CreateSurgery(DateTime start, DateTime end, Doctor doctor, Patient patient, Room room, bool isUrgent);
+        Task<AppointmentRequest> CreateAppointmentRequest(DateTime start, DateTime end, Patient patient, Doctor requester, Doctor proposedDoctor, DateTime timestamp, Room room);
+        Task<AppointmentRequest> CreateSurgeryRequest(DateTime start, DateTime end, Patient patient, Doctor requester, Doctor proposedDoctor, bool isUrgent, DateTime timestamp, Room room);
+        Task<bool> CancelCalendarEntry(CalendarEntry calendarEntry);
     }
 }
