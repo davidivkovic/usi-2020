01342465a46f7bc3629a7b764f9e9dfa6bd7ddbb
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-16 06:16:46 +0200

    Fixed Lazy loading queries in the NotificationService

--- HospitalCalendar/HospitalCalendar.EntityFramework/Services/NotificationServices/NotificationService.cs
+++ HospitalCalendar/HospitalCalendar.EntityFramework/Services/NotificationServices/NotificationService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using HospitalCalendar.Domain.Models;
 using HospitalCalendar.Domain.Services;
 using HospitalCalendar.Domain.Services.NotificationsServices;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace HospitalCalendar.EntityFramework.Services.NotificationServices
 {
@@ -10,10 +12,89 @@ namespace HospitalCalendar.EntityFramework.Services.NotificationServices
     {
         public NotificationService(HospitalCalendarDbContextFactory contextFactory) : base(contextFactory)
         {
-
         }
+
+        public new async Task<ICollection<Notification>> GetAll()
+        {
+            var surgeryNotifications = await GetAllSurgeryNotifications();
+            var surgeryRequestNotifications = await GetAllSurgeryRequestNotifications();
+            var appointmentRequestNotifications = await GetAllAppointmentRequestNotifications();
+            var appointmentChangeRequestNotificationsRequest = await GetAllAppointmentChangeRequestNotifications();
+
+            var notifications = new List<Notification>();
+            notifications.AddRange(surgeryNotifications);
+            notifications.AddRange(surgeryRequestNotifications);
+            notifications.AddRange(appointmentRequestNotifications);
+            notifications.AddRange(appointmentChangeRequestNotificationsRequest);
+            return notifications;
+        }
+        public async Task<ICollection<SurgeryNotification>> GetAllSurgeryNotifications()
+        {
+            await using var context = ContextFactory.CreateDbContext();
+            return await context.SurgeryNotifications
+                .Include(surgeryNotification => surgeryNotification.Surgery)
+                .ThenInclude(surgery => surgery.Doctor)
+                .Include(surgeryNotification => surgeryNotification.Surgery)
+                .ThenInclude(surgery => surgery.Patient)
+                .Include(surgeryNotification => surgeryNotification.Surgery)
+                .ThenInclude(surgery => surgery.Type)
+                .Include(surgeryNotification => surgeryNotification.Surgery)
+                .ThenInclude(surgery => surgery.Room)
+                .ToListAsync();
+        }
+
+        public async Task<ICollection<SurgeryRequestNotification>> GetAllSurgeryRequestNotifications()
+        {
+            await using var context = ContextFactory.CreateDbContext();
+            return await context.SurgeryRequestNotifications
+                .Include(surgeryRequestNotification => surgeryRequestNotification.SurgeryRequest)
+                .ThenInclude(surgeryRequest => surgeryRequest.Patient)
+                .Include(surgeryRequestNotification => surgeryRequestNotification.SurgeryRequest)
+                .ThenInclude(surgeryRequest => surgeryRequest.Room)
+                .Include(surgeryRequestNotification => surgeryRequestNotification.SurgeryRequest)
+                .ThenInclude(surgeryRequest => surgeryRequest.Requester)
+                .Include(surgeryRequestNotification => surgeryRequestNotification.SurgeryRequest)
+                .ThenInclude(surgeryRequest => surgeryRequest.ProposedDoctor)
+                .ThenInclude(doctor => doctor.Specializations)
+                .ToListAsync();
+        }
+
+        public async Task<ICollection<AppointmentRequestNotification>> GetAllAppointmentRequestNotifications()
+        {
+            await using var context = ContextFactory.CreateDbContext();
+            return await context.AppointmentRequestNotifications
+                .Include(appointmentRequestNotification => appointmentRequestNotification.AppointmentRequest)
+                .ThenInclude(appointmentRequest => appointmentRequest.Requester)
+                .Include(appointmentRequestNotification => appointmentRequestNotification.AppointmentRequest)
+                .ThenInclude(appointmentRequest => appointmentRequest.ProposedDoctor)
+                .Include(appointmentRequestNotification => appointmentRequestNotification.AppointmentRequest)
+                .ThenInclude(appointmentRequest => appointmentRequest.Patient)
+                .Include(appointmentRequestNotification => appointmentRequestNotification.AppointmentRequest)
+                .ThenInclude(appointmentRequest => appointmentRequest.Room)
+                .ToListAsync();
+        }
+        public async Task<ICollection<AppointmentChangeRequestNotification>> GetAllAppointmentChangeRequestNotifications()
+        {
+            await using var context = ContextFactory.CreateDbContext();
+            return await context.AppointmentChangeRequestNotifications
+                .Include(appointmentChangeRequestNotification => appointmentChangeRequestNotification.AppointmentChangeRequest)
+                .ThenInclude(appointmentChangeRequest => appointmentChangeRequest.Appointment)
+                .ThenInclude(appointment => appointment.Doctor)
+                .Include(appointmentChangeRequestNotification => appointmentChangeRequestNotification.AppointmentChangeRequest)
+                .ThenInclude(appointmentChangeRequest => appointmentChangeRequest.Appointment)
+                .ThenInclude(appointment => appointment.Patient)
+                .Include(appointmentChangeRequestNotification => appointmentChangeRequestNotification.AppointmentChangeRequest)
+                .ThenInclude(appointmentChangeRequest => appointmentChangeRequest.Appointment)
+                .ThenInclude(appointment => appointment.Type)
+                .Include(appointmentChangeRequestNotification => appointmentChangeRequestNotification.AppointmentChangeRequest)
+                .ThenInclude(appointmentChangeRequest => appointmentChangeRequest.Appointment)
+                .ThenInclude(appointment => appointment.Room)
+                .ToListAsync();
+        }
+
         public async Task<AppointmentChangeRequestNotification> PublishAppointmentChangeRequestNotification(AppointmentChangeRequest appointmentChangeRequest, DateTime timestamp, string message)
         {
+
             var notification = new AppointmentChangeRequestNotification();
             await Create(notification);
             notification.IsActive = true;
