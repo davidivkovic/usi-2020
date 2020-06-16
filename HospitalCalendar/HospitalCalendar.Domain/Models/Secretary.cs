505bd35ee07946148edf35a649713fa2731eb3e6
David Ivkovic <ivkovic.sw61.2019@uns.ac.rs>
2020-06-16 06:12:41 +0200

    Removed navigation properties for Secretary

--- HospitalCalendar/HospitalCalendar.Domain/Models/Secretary.cs
+++ HospitalCalendar/HospitalCalendar.Domain/Models/Secretary.cs
@@ -1,11 +1,7 @@
-using System.Collections.Generic;
 
 namespace HospitalCalendar.Domain.Models
 {
     public class Secretary : User
     {
-        public virtual ICollection<AppointmentRequestNotification> AppointmentRequestNotifications { get; set; }
-        public virtual ICollection<SurgeryNotification> SurgeryNotifications { get; set; }
-        public virtual ICollection<AppointmentChangeRequestNotification> AppointmentChangeRequestNotifications { get; set; }
     }
 }
